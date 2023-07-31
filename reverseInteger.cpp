a7a7428dbe84b6f7af47e42ac1f25212b0770500
Malhar <email@example.com>
2023-07-31 15:15:13 +0530

    Added reverseInteger.cpp script and added some images in images folder

---
+++ reverseInteger.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int getReverse(int n){
+    while(n>0){
+        int lastdigit=n%10;
+        ans=(ans*10)+lastdigit;
+        n=n/10;
+    }
+    return ans;
+}
+int main(){
+int n;
+cout<<"Enter a number: ";
+cin>>n;
+int ans=getreverse(n);
+cout<<"Reverse of number is: "<<ans<<endl;
+return 0;
+}
